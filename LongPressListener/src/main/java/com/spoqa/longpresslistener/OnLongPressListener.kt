436b98cf8add8aa89d9e0ae96287614e388716ad
Leah <leah@spoqa.com>
2020-09-16 18:31:14 +0900

    Create OnLongPressListener that handles long press event with custom duration

---
+++ LongPressListener/src/main/java/com/spoqa/longpresslistener/OnLongPressListener.kt
@@ -0,0 +1,61 @@
+package com.spoqa.longpresslistener
+
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * Called when a long press event with certain dutation is dispatched to a view
+ *
+ * @param onLongPressHandler Handles the long press action
+ * @param duration Certain duration for handling long press after delay
+ *                 (Default value is 3 seconds)
+ */
+class OnLongPressListener @JvmOverloads constructor(
+    private val onLongPressHandler: OnLongPressHandler,
+    private val duration: Long = 3000L
+) : View.OnTouchListener {
+
+    var longClickActionMoveOutsideFlag = false
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        onLongPressHandler.apply {
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    v.isPressed = true
+                    hasPerformedLongPress = false
+                    postCheckForLongPress(duration)
+                    longClickActionMoveOutsideFlag = false
+                }
+                MotionEvent.ACTION_CANCEL -> {
+                    v.isPressed = false
+                    if (!hasPerformedLongPress) removeLongPressCallback()
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    // Don't invoke onLongPressCallback when the touch area
+                    // is moved outside current view
+                    if (event.x < 0 || event.y < 0 ||
+                        event.x > v.measuredWidth || event.y > v.measuredHeight) {
+                        v.isPressed = false
+                        longClickActionMoveOutsideFlag = true
+                        if (!hasPerformedLongPress) {
+                            removeLongPressCallback()
+                        }
+                    } else {
+                        longClickActionMoveOutsideFlag = false
+                    }
+                }
+                MotionEvent.ACTION_UP -> {
+                    v.isPressed = false
+                    if (!hasPerformedLongPress) {
+                        removeLongPressCallback()
+                        // Same as onClick Action
+                        if (!longClickActionMoveOutsideFlag) {
+                            v.performClick()
+                        }
+                    }
+                }
+            }
+        }
+        return true
+    }
+}
