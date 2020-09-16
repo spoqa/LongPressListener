c5cbf0f8dbb4f4c4ad8a0912c9563e201e2ac8dc
Leah <leah@spoqa.com>
2020-09-16 18:21:22 +0900

    Add interface for a callback to be invoked after long press event

---
+++ LongPressListener/src/main/java/com/spoqa/longpresslistener/OnLongPressCallback.kt
@@ -0,0 +1,9 @@
+package com.spoqa.longpresslistener
+
+/**
+ * Interface definition for a callback to be invoked when a long press event is
+ * dispatched to this view.
+ */
+interface OnLongPressCallback {
+    fun onPress()
+}
